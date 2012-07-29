81356e74f8df21ee2cfcb085903dedf5ba059450
Dean Murphy <deano.uk@gmail.com>
2012-07-29 12:52:09 +0100

    Added help menu - included assets for help menu. Signed-off-by:Dean Murphy <deano.uk@gmail.com>

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 -- Crafted by @CraftyDeano
 
+-- Crafted by @CraftyDeano
+
 -- Hide Status Bar
 
 display.setStatusBar(display.HiddenStatusBar)
@@ -19,6 +21,7 @@ system.setAccelerometerInterval( 100 )
 local menuScreenGroup	-- display.newGroup()
 local mmScreen
 local playBtn
+local helpBtn
 
 -- Game Screen
 
@@ -75,16 +78,23 @@ function mainMenu()
     
     playBtn = display.newImage("img/playbtn.png")
     playBtn:setReferencePoint( display.CenterReferencePoint )
-    playBtn.x = _W; playBtn.y = _H + 50
+    playBtn.x = _W - 100; playBtn.y = _H + 50
     playBtn.name = "playbutton"
     
+    helpBtn = display.newImage("img/helpbtn.png")
+    helpBtn:setReferencePoint( display.CenterReferencePoint )
+    helpBtn.x = _W + 100; helpBtn.y = _H + 50
+    helpBtn.name = "helpbutton"
+    
     menuScreenGroup:insert( mmScreen )
-    menuScreenGroup:insert( playBtn )
+    menuScreenGroup:insert( playBtn )       
+    menuScreenGroup:insert( helpBtn )
     menuScreenGroup:setReferencePoint( display.CenterReferencePoint )
 
     
     playBtn:addEventListener("tap", loadGame)
-    
+    helpBtn:addEventListener("tap", helpGame)
+
 end
 
 -- loads the game
@@ -92,6 +102,15 @@ function loadGame(event)
     if event.target.name == "playbutton" then
         transition.to(menuScreenGroup,{time = 750, alpha = 0, xScale = 2.5, yScale = 2.5, rotation = 720, onComplete = addGameScreen})
         playBtn:removeEventListener( "tap", loadGame )
+        helpBtn:removeEventListener( "tap", helpGame )
+    end
+end
+
+function helpGame(event)
+    if event.target.name == "helpbutton" then
+        transition.to(menuScreenGroup,{time = 750, alpha = 0, xScale = 0, yScale = 0, rotation = 720, onComplete = addGameScreen})
+        playBtn:removeEventListener( "tap", loadGame )
+        helpBtn:removeEventListener( "tap", helpGame )
     end
 end
 
