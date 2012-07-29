520da2690889ccacc7847ea72fb5e82a51bd7c5a
Dean Murphy <deano.uk@gmail.com>
2012-07-29 22:18:07 +0100

    added alert box for game end.

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-CiderRunMode = {};CiderRunMode.runmode = true;CiderRunMode.assertImage = true;require "CiderDebugger";
+
 
 -- Crafted by @CraftyDeano
 
@@ -217,3 +217,35 @@ function gameLevel2()
         end
     end
 end    
+
+
+-- alert screen for gameend
+function alertScreen(title, message)
+    
+    alertBox = display.newImage("img/alertBox.png")
+    alertBox.x = 240
+    alertBox.y = 160
+    
+    transition.from(alertBox, {time = 500, xScale = 0.5, yScale = 0.5, transition = easing.outExpo})
+    
+    conditonDisplay = display.newText(title,0,0,"Arial",38)
+    conditionDisplay:setTextColor( 255, 255, 255, 255 ) 
+    conditionDisplay.xScale = 0.5
+    conditionDisplay.yScale = 0.5
+    conditionDisplay:setReferencePoint( display.CenterReferencePoint )
+    conditionDisplay.x = display.contentCenterX
+    conditionDisplay.y = display.contentCenterY - 15
+    
+    messageText = display.newText(message,0,0,"Arial",24)
+    messageText:setTextColor( 255, 255, 255, 255 ) 
+    messageText.xScale = 0.5
+    messageText.yScale = 0.5
+    messageText:setReferencePoint( display.CenterReferencePoint )
+    messageText.x = display.contentCenterX
+    messageText.y = display.contentCenterY + 15
+    
+    alertDisplayGroup = display.newGroup()
+    alertDisplayGroup:insert(alertBox)
+    alertDisplayGroup:insert(conditionDisplay)
+    alertDisplayGroup:insert(messageText)
+end
